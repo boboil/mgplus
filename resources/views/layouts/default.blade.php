8110c0e45c2a594d4ca88bc54ce942ab34be7a73
YOdarchenko <yevhen.odarchenko@agnizar.com>
2022-04-02 17:08:45 +0300

    css version

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{$lang}}">
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=1"/>
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"/>
     <!--Style Google Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;display=swap&amp;subset=cyrillic,cyrillic-ext"/>
-    <link rel="stylesheet" href="{{asset('style.css')}}"/>
+    <link rel="stylesheet" href="{{asset('style.css?v=1')}}"/>
     <link rel="stylesheet" href="{{asset('css/back_style.css')}}"/>
     <!--Script-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
